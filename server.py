f7e1b1d0637675d5639584e956b462b1d67f0380
Denis Humeniuk <mail05denis@gmail.com>
2024-12-19 12:12:40 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from datetime import datetime, timedelta
@@ -88,4 +89,5 @@ def receive_events():
         return jsonify({"message": "An error occurred.", "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
